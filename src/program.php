7fe82bf08f8af388783f19e54482a071f84fa06d
sarvesh shukla <sarveshshukla@cedcoss.com>
2022-02-15 11:51:53 +0530

    chess

--- src/program.php
+++ src/program.php
@@ -1,21 +1,33 @@
-<?php  
-$num = 4;  
-$factorial = 1;  
-for ($x=$num; $x>=1; $x--)   
-{  
-  $factorial = $factorial * $x;  
-}  
-echo "Factorial of $num is $factorial";  
-?>  
+
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+     <html> 
+     <head> 
+  <title></title>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  </head>
+  <body> 
+  <h3>Chess 400px X 30px </h3>
+   <table width="400px" cellspacing="0px" cellpadding="0px" border="1px">
+   <!-- cell 270px wide (8 columns x 60px) -->
+      <?php
+      for($row=1;$row<=8;$row++)
+	  {
+          echo "<tr>";
+          for($col=1;$col<=8;$col++)
+		  {
+          $total=$row+$col;
+          if($total%2==0)
+		  {
+          echo "<td height=30px width=30px bgcolor=#FFFFFF></td>";
+          }
+		  else
+		  {
+          echo "<td height=30px width=30px bgcolor=#000000></td>";
+          }
+          }
+          echo "</tr>";
+    }
+          ?>
+  </table>
+  </body>
+  </html>
